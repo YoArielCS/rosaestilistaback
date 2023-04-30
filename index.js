b2959364312737b99544f175d173d99350c007fe
YoArielCS <arielalvarezcr@gmail.com>
2023-04-29 22:52:06 -0300

    mi primer commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const http = require("http")
+function requestController() {
+    //logica de nuestra funcion
+    console.log("Hola Mundo")
+}
+//configurar nuetra funcion
+const server = http.createServer(requestController)
+server.listen(4000)
