b2fe85d9d77d3f5aec6923e45c93eca265a13b25
YoArielCS <arielalvarezcr@gmail.com>
2023-05-01 00:54:55 -0300

    Agregamos el Script 'start'

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("http")
 function requestController() {
     //logica de nuestra funcion
-    console.log("Hola Mundos!!!!!!")
+    console.log("Hola Mundos!!!")
 }
 //configurar nuetra funcion
 const server = http.createServer(requestController)
