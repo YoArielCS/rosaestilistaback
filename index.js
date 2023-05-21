73a46a0d1aa5758411bc3f144aaf3be855dfa6d8
YoArielCS <arielalvarezcr@gmail.com>
2023-05-21 02:14:02 -0300

    se actualizo imagenes

--- index.js
+++ index.js
@@ -3,10 +3,21 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const Schema = mongoose.Schema
 
 mongoose.connect(process.env.MONGODB_URL).then(() => {
     console.log("Conexion exitosa con la base de datos!!")
 }).catch((err) => console.log("Hubo un error en la conexion con la BD", { err }))
+// Definir un esquema para el modelo de datos del formulario de contacto
+const contactSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    comment: String,
+})
+// Crear un modelo de mongoose basado en el esquema
+const Contact = mongoose.model('Contact', contactSchema, 'Contactos')
+
 // servir archivos estaticos
 app.use(express.static('public'))
 app.use(express.json())
