3e7474252630cc70c2eabd931c62b079eb97f95e
YoArielCS <arielalvarezcr@gmail.com>
2023-05-20 21:59:26 -0300

    Conexion a MongoDB

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ require("dotenv").config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    console.log("Conexion exitosa con la base de datos!!")
+}).catch((err) => console.log("Hubo un error en la conexion con la BD", { err }))
 // servir archivos estaticos
 app.use(express.static('public'))
+app.use(express.json())
 
 //configurar rutas
 app.get('/', (req, res) => {
